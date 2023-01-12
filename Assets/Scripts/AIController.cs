5accf630a29a451ec1b72e8566ba6804c4ffce02
RicardoLuqueMartos <lmricmail@yahoo.fr>
2023-01-12 16:12:27 +0100

    customize Difficulty and gameplay UI info ready

--- Assets/Scripts/AIController.cs
+++ Assets/Scripts/AIController.cs
@@ -64,7 +64,7 @@ public class AIController : MonoBehaviour
             }
         }
     }
-    public void CheckCode()
+    public void CheckCode() // Gilbert + Nicolas + Moi
     {
         Debug.Log("Verification de ligne");
         bool[] goodColors = new bool[boardManager.GameSettings.Rules.slotsByLine];
@@ -146,7 +146,91 @@ public class AIController : MonoBehaviour
             }
         }
     }
-  
+
+    public void CompareRows() // Vlad
+    {
+/*
+        int wrongPosition = 0;
+        int samePlace = 0;
+        List<int> alreadyCounted = new List<int>();
+        List<int> alreadyCountedColors = new List<int>();
+
+        for (int i = 0; i < boardManager.GameSettings.Rules.slotsByLine; i++)
+        {
+            for (int j = 0; j < boardManager.GameSettings.Rules.slotsByLine; j++)
+            {
+                if (boardManager.IALine.slotsList[i].GetComponent<MeshRenderer>().material.color 
+                    == boardManager.Board.linesList[boardManager.CurrentLine].slotsList[j].GetComponent<MeshRenderer>().material.color)
+                {
+                    if (i == j && !alreadyCounted.Contains(i))
+                    {
+                        samePlace++;
+                        alreadyCounted.Add(i);
+                        alreadyCountedColors.Add(j);
+                    }
+                    else if (i != j && !alreadyCountedColors.Contains(j) && !alreadyCounted.Contains(i))
+                    {
+                        wrongPosition++;
+                        alreadyCountedColors.Add(j);
+                        alreadyCounted.Add(i);
+                    }
+                }
+            }
+        }
+        //    OnCompare?.Invoke(wrongPosition, samePlace);
+
+        for (int i = 0; i < nbGoodColors; i++)
+        {
+            boardManager.Board.linesList[boardManager.CurrentLine].miniSlotsXManager.MiniBallSlotsList[i].ballGameObject.GetComponent<MeshRenderer>().material
+                = boardManager.GameSettings.Colors.BlackColor;
+
+            boardManager.Board.linesList[boardManager.CurrentLine].miniSlotsXManager.MiniBallSlotsList[i].ballGameObject.SetActive(true);
+
+            currentResultBallsIndex++;
+        }
+
+        // win game ?
+        if (nbGoodColors == boardManager.GameSettings.Rules.slotsByLine)
+        {
+            Win();
+        }
+        else if (boardManager.CurrentLine == boardManager.GameSettings.Rules.linesByBoard
+            && nbGoodColors == boardManager.GameSettings.Rules.slotsByLine)
+        {
+            Loose();
+        }
+        else
+        {
+            GoToNextLine();
+        }*/
+    }
+
+    private void Compare() // Florent
+    {
+        /*
+        List<int> TempSol = Solution.ToList();
+        List<int> TempGuess = UserGuess.ToList();
+
+        for (int i = 0; i < UserGuess.Length; i++)
+        {
+            if (UserGuess[i] == Solution[i])
+            {
+                TempSol.Remove(UserGuess[i]);
+                TempGuess.Remove(UserGuess[i]);
+                _Goods++;
+            }
+        }
+
+        for (int i = 0; i < TempGuess.Count; i++)
+        {
+            if (TempSol.Contains(TempGuess[i]))
+            {
+                TempSol.Remove(TempGuess[i]);
+                _Wrongs++;
+            }
+        }*/
+    }
+
     private void GoToNextLine()
     {
         SelectNextLine?.Invoke();
